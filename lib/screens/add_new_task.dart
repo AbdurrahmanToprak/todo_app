a6cb38031573dab338cb576b2130cc84e68e56f3
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-01-09 23:34:38 +0300

    add_new_task sayfası eklendi.

---
+++ lib/screens/add_new_task.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewTaskScreen extends StatelessWidget {
+  const AddNewTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(),
+            body: Column(
+              children: [Container()],
+            )));
+  }
+}
