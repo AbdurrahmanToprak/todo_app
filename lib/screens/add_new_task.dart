79791e22d947e5da946a795386fdaa53b4bb9b39
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-01-09 23:50:23 +0300

    add_new_task sayfasının header kısmı yapıldı, home sayfasına geri dönüş sağlandı.

--- lib/screens/add_new_task.dart
+++ lib/screens/add_new_task.dart
@@ -5,11 +5,45 @@ class AddNewTaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
     return SafeArea(
         child: Scaffold(
-            appBar: AppBar(),
             body: Column(
-              children: [Container()],
-            )));
+      children: [
+        Container(
+          width: deviceWidth,
+          height: deviceHeight / 10,
+          decoration: BoxDecoration(
+              color: Colors.blue,
+              image: DecorationImage(
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                  image: const AssetImage("lib/assets/images/headerimage.png"),
+                  fit: BoxFit.cover)),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.close),
+                iconSize: 40,
+                color: Colors.white,
+              ),
+              const Expanded(
+                  child: Text(
+                "Add New Task",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ))
+            ],
+          ),
+        )
+      ],
+    )));
   }
 }
